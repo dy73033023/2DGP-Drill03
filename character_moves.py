9138fd5efc870034a41c2fcef28d06432ea4caa5
dy73033023 <dy73033023@tukorea.ac.kr>
2025-09-16 11:02:31 +0900

    캐릭터 출력 구현 완료

--- character_moves.py
+++ character_moves.py
@@ -2,19 +2,25 @@ from pico2d import *
 
 open_canvas()
 
+boy = load_image("character.png")
+
 def move_rectangle():
     print("move_rectangle")
-
+    pass
 
 
 def move_circle():
     print("move_circle")
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
+    pass
 
 
 while True:
-    move_rectangle()
     move_circle()
-    
+    move_rectangle()
+
     pass #패스
 
 close_canvas()
